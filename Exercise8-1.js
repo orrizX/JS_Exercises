470b6e80a1c8f5791910affd230da1d573a8450a
orrizX <zo292@nyu.edu>
2021-11-02 12:10:45 -0400

    Create Exercise8-1.js

---
+++ Exercise8-1.js
@@ -0,0 +1,9 @@
+let word = prompt("Enter a word:");
+
+const len = word.length;
+const lowercaseWord = word.toLowerCase();
+const uppercaseWord = word.toUpperCase();
+
+console.log(len);
+console.log(lowercaseWord);
+console.log(uppercaseWord);
