d6fa09c666c5e6ddbac2208633dae012406585ae
orrizX <zo292@nyu.edu>
2021-11-01 22:29:37 -0400

    Update Exercise6-1.js

--- Exercise6-1.js
+++ Exercise6-1.js
@@ -2,10 +2,14 @@ const aurora = {
     name: "Aurora",
     health: 150,
     strength: 25,
-    xp: 0
+    xp: 0,
+
+    describe() {
+      return `${aurora.name} has ${aurora.health} health points, ${aurora.strength} as strength and ${aurora.xp} XP points`;
+    }
   };
   
-  console.log(`${aurora.name} has ${aurora.health} health points, ${aurora.strength} as strength and ${aurora.xp} XP points`);
+  console.log(aurora.describe());
   
   // Aurora is harmed by an arrow
   aurora.health -= 20;
@@ -16,4 +20,4 @@ const aurora = {
   // Aurora defeats a boss
   aurora.xp += 15;
   
-  console.log(`${aurora.name} has ${aurora.health} health points, ${aurora.strength} as strength and ${aurora.xp} XP points`);
+  console.log(aurora.describe());
