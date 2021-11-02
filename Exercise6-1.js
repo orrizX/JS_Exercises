c6f782e0175a08800c49596b27c294fc73955d89
orrizX <zo292@nyu.edu>
2021-11-01 20:59:25 -0400

    Create Exercise6-1.js

---
+++ Exercise6-1.js
@@ -0,0 +1,19 @@
+const aurora = {
+    name: "Aurora",
+    health: 150,
+    strength: 25,
+    xp: 0
+  };
+  
+  console.log(`${aurora.name} has ${aurora.health} health points, ${aurora.strength} as strength and ${aurora.xp} XP points`);
+  
+  // Aurora is harmed by an arrow
+  aurora.health -= 20;
+  
+  // Aurora equips a strength necklace
+  aurora.strength += 10;
+
+  // Aurora defeats a boss
+  aurora.xp += 15;
+  
+  console.log(`${aurora.name} has ${aurora.health} health points, ${aurora.strength} as strength and ${aurora.xp} XP points`);
