df0f16ae939c5a676e8b27606abc523d2c948fc3
orrizX <zo292@nyu.edu>
2021-11-01 23:45:59 -0400

    Update Exercise_in_class.js

--- Exercise_in_class.js
+++ Exercise_in_class.js
@@ -1,8 +1,17 @@
 const account = {
     name: "Alex",
     balance: 0,
-    credit: 75,
+    credit: 0,
+    debit: 0,
     describe(){
-        return ``
+        return `owner: ${account.name}, balance ${account.balance}`
     }
 }
+
+console.log(account.describe());
+
+account.credit += 250;
+account.debit += 80;
+account.balance = account.credit - account.debit;
+
+console.log(account.describe());
