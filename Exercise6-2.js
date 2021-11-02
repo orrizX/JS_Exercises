3b3b588d8285b70669f9910bb2eda45b2877bd87
orrizX <zo292@nyu.edu>
2021-11-01 22:33:42 -0400

    Create Exercise6-2.js

---
+++ Exercise6-2.js
@@ -0,0 +1,11 @@
+const dog = {
+    name: "Fang",
+    species: "boarhound",
+    size: 75,
+    bark(){
+        return `Grrr! Grrr!`;
+    }
+  };
+
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
